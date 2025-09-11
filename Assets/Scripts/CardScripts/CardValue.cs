574610fa31af8aac3b8fa0f0de80735a18d67cb2
s9716903 <luos97169@gmail.com>
2025-09-11 20:34:42 +0800

    design card system

---
+++ Assets/Scripts/CardScripts/CardValue.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class CardValue :MonoBehaviour
+{
+    [Header("CardPart")]
+    public GameObject Cardfront;
+    public GameObject Cardback;
+    public GameObject Cardresult;
+    public Image Blacktypeimage;
+    public Image Graytypeimage;
+    //public TextMeshProUGUI CardUpValueText;
+    //public TextMeshProUGUI CardDownValueText;
+    public Image Resultimage;
+    public Image Interactableimage;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
